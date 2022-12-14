c8f064b8e00dfab4d64ef84c27b725f425e72011
opilane <opilane@HP-CZC5261P8F>
2022-12-14 15:37:54 +0200

    111

--- Praktika.py
+++ Praktika.py
@@ -1,1 +1,24 @@
+from math import *
+from random import *
+a=int(input("sissesta arv: "))
 
+
+
+
+
+
+
+try:
+    a=int(input("sissesta arv: "))
+    if a>0:
+        print("Positiivne")
+        if a%2==0:
+            print(f"{a} on paaris")
+        else:
+            print(f"{a} on paaritu")
+    else:
+        print("Negatiivne")
+except:
+    print("Vale andmetüüp")
+
+    
